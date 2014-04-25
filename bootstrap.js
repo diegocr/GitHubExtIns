3319dcf00714411b4166a6a1be56853bf1159c7e
Noitidart <noitidart@gmail.com>
2014-04-24 21:18:08 -0700

    1.5a3.rev2
    -Copy paste of yajd commit #62 (https://github.com/yajd/GitHubExtIns/blob/0a677ff88f1a361ded052beb068016be462a1c95/bootstrap.js)
    -Cut out the extraneous code running when adding button to editable page
    -Cleaned for PullRequest (PR)

--- bootstrap.js
+++ bootstrap.js
@@ -16,9 +16,8 @@ Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/FileUtils.jsm");
 Cu.import("resource://gre/modules/AddonManager.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-Cu.import("resource://gre/modules/devtools/Console.jsm");
 Cu.import("resource://gre/modules/osfile.jsm");
-const { TextEncoder, TextDecoder } = Cu.import("resource://gre/modules/commonjs/toolkit/loader.js", {});
+const { TextEncoder } = Cu.import("resource://gre/modules/commonjs/toolkit/loader.js", {});
 
 var TEncoder;
 
@@ -80,8 +79,7 @@ function onClickHanlder(ev) {
 		return;
 	}
 	
-	if (this.classList.contains('disabled')) {
-		console.log('button is disabled so exit');
+	if (this.classList.contains('disabled')) { //needed for when checking if editable file is installable
 		return;
 	}
 
@@ -115,16 +113,10 @@ function onClickHanlder(ev) {
 					zipWriter = Cc["@mozilla.org/zipwriter;1"]
 							.createInstance(Ci.nsIZipWriter);
 
-				let oFile = FileUtils.getFile("TmpD", [addon.tag+'.xpi']);
-				console.log('pre zip open');
-				console.log('this.getAttribute(path) = ', this.getAttribute('path'));
-				
+				let oFile = FileUtils.getFile("TmpD", [addon.tag+'.xpi']);				
 				
 				zipReader.open(nFile);
 				zipWriter.open(oFile, 0x2c);
-				
-				console.log('zip opened');
-
 				
 				let p = (this.getAttribute('path') || "*/"),
 					m = zipReader.findEntries(p + "*");
@@ -133,9 +125,7 @@ function onClickHanlder(ev) {
 				if (this.hasAttribute('filepath')) {
 					var fileName = this.getAttribute('filepath');
 					var useUncommitedFilePath = this.getAttribute('filepath').replace(this.getAttribute('path'), ''); //relative to path, because thats what is getting written to xpi
-					fileName = fileName.substr(fileName.lastIndexOf('/')+1);
-					console.log('the filename is = ', fileName);
-					
+					fileName = fileName.substr(fileName.lastIndexOf('/')+1);					
 					var tmpFileOfUncommitedFile = new FileUtils.File(oFile.parent.path + '\\' + fileName)
 				}
 				
@@ -155,8 +145,7 @@ function onClickHanlder(ev) {
 
 					} else {
 						if (useUncommitedFilePath && n == useUncommitedFilePath) {
-							console.log('was writing to zip n, n =', n);
-							console.log('but we are in edit page of this so dont add this file from zip, we will create file out of that and add it after this while loop');
+							//skip adding file
 						} else {							
 							zipWriter.addEntryStream(n, e.lastModifiedTime,
 								Ci.nsIZipWriter.COMPRESSION_FASTEST,
@@ -168,7 +157,6 @@ function onClickHanlder(ev) {
 				var btn = this;
 				
 				var postZipWrite = function() {
-					console.log('zip closed');
 					
 					zipReader.close();
 					zipWriter.close();
@@ -195,16 +183,7 @@ function onClickHanlder(ev) {
 							f.style.animation = null;
 							f.className = f.className.replace('hourglass',c);
 							iNotify(aAddon, aMsg, aResult => {
-								//oFile.remove(!1);
-								var promiseOFileRemove = OS.File.remove(oFile.path);
-								promiseOFileRemove.then(
-									function onsuc() {
-										console.log('succesfully deleted ofile', oFile.path);
-									},
-									function(aRejectReason) {
-										console.error('promiseRemove ofile failed = ', aRejectReason);
-									}
-								);
+								OS.File.remove(oFile.path);
 
 								if(aResult !== null && aAddon && aAddon.pendingOperations) {
 									let m = aAddon.name + ' requires restart.\n\n'
@@ -251,21 +230,11 @@ function onClickHanlder(ev) {
 						aInstall.install();
 					});
 
-					//nFile.remove(!1); //should probably change to use OS.File so should be OS.File.remove(nFile.path);
-					var promiseRemove = OS.File.remove(nFile.path);
-					promiseRemove.then(
-						function onsuc() {
-							console.log('succesfully deleted nfile', nFile.path);
-						},
-						function(aRejectReason) {
-							console.error('promiseRemove nfile failed = ', aRejectReason);
-						}
-					);
+					OS.File.remove(nFile.path);
 				};
 				var postZipWriteBinded = postZipWrite.bind(this);
 				
 				if (this.hasAttribute('filepath')) {
-							console.log('now creating file out of file on edit page and then will add it in');
 							if (!TEncoder) {
 								TEncoder = new TextEncoder(); // This encoder can be reused for several writes
 							}
@@ -275,30 +244,15 @@ function onClickHanlder(ev) {
 							});
 							promiseCreateUncommitedFile.then(
 								function() {
-									console.log('uncommited file succesfully created so now adding this file to zip then doing postZipWriteBinded');
 									zipWriter.addEntryFile(useUncommitedFilePath,
 										Ci.nsIZipWriter.COMPRESSION_FASTEST,
 										tmpFileOfUncommitedFile, !1);
 									
-									
-										var promiseRemoveUncommitedFile = OS.File.remove(tmpFileOfUncommitedFile.path);
-										promiseRemoveUncommitedFile.then(
-											function() {
-												console.log('succesfully deleted file', tmpFileOfUncommitedFile.path);
-											},
-											function(aRejectReason) {
-												console.warn('removing uncommited file from tmp failed = ', aRejectReason);
-											}
-										);
-									
+									OS.File.remove(tmpFileOfUncommitedFile.path);									
 									postZipWriteBinded();
-								},
-								function(aRejectReason) {
-									console.error('creating file of uncommited file failed');
 								}
 							);
 				} else {
-					console.log('doing postZipWriteBinded right away as no need for promise');
 					postZipWriteBinded();
 				}
 				
@@ -401,72 +355,22 @@ function onPageLoad(doc) {
 		}
 	}
 	else if (editForm && editForm.hasAttribute('action')) {
-		//add disabled button saying it is checking parent dirs for install.rdf
-		var filePath = editForm.getAttribute('action'); //'https://github.com/yajd/XPICompiler/tree-save/master/release/bootstrap.js';
-		/*
-		//start searching parent dirs till find intall.rdf, first install.rdf it finds it will install that and edit this file into it
-		var determineAndloadDir = function(argFilePath) {
-			if (argFilePath[0] == '/') {
-				var spliceOrigin = true;
-				argFilePath = argFilePath.slice(1);
-			}
-			var pathSplit = argFilePath.split('/');
-			if (spliceOrigin) {
-			   pathSplit.splice(0, 0, doc.defaultView.location.origin);
-			}
-			dirPath = pathSplit.slice(0, pathSplit.length - 1).join('/');
-			var isRootDir = false;
-			if (pathSplit[pathSplit.length - 2] == 'master') {
-				//this is root dir so after loadDir, if still no rdf then give up
-				isRootDir = true;
-			}
-			loadDir(dirPath, isRootDir);
-		}
-		
-		var loadDir = function(argDirPath, argIsRootDir) {
-			var cb = function(responseText) {
-				lookForInstallRdfAndMakeButton(responseText, argDirPath, argIsRootDir);
-			};
-			xhr(argDirPath, cb);
-		}
-		
-		var lookForInstallRdfAndMakeButton = function(argResponseText, argDirPath, argIsRootDir) {
-			//if src has install.rdf then do addButton else if !argIsRootDirPath then determineAndloadDir(argDirPath) ELSE give up
-			console.log(argResponseText);
-		}
-		
-		determineAndloadDir(filePath);
-		//start searching for rdf
-		*/
+		var filePath = editForm.getAttribute('action');
 		let c = 7, n, z;
-		console.log('looking for cloning node');
 		while(c-- && !(n=doc.querySelector('a.minibutton:nth-child('+c+')')));
 
 		if(n && n.textContent.trim() === 'Download ZIP') {
-			console.log('cloning node found');
 			c = doc.querySelector('div.only-with-full-nav');
-
-			if(!c || doc.defaultView.getComputedStyle(c).getPropertyValue('display') == 'block') {
-				console.log('doing addButton');
-				addButton(n);
-				console.log('done adding button');
-			} else {
-				console.log('c not found');
-				z = n;
-				n = 0;
-			}
+			z = n;
+			n = 0;
 		}
 
 		if(!n) {
-			console.log('n not found');
-			console.log('so looking for breadcrumb');
 			n = doc.querySelector('div.breadcrumb');
 			n = n && n.firstElementChild;
 
 			if( n ) {
-				console.log('done adding button breadcrumb');
 				var btn = addButton(n,z);
-				//btn.setAttribute('path', );
 				btn.className += ' danger disabled'
 				var l = btn.lastChild;
 				var f = btn.firstChild;
@@ -478,17 +382,13 @@ function onPageLoad(doc) {
 				for (var i=1; i<breadcrumbs.length-1; i++) { //start at i=1 because not possible to have */install.rdf or any files */ because zips off of github first hold a folder
 					breads.push(breadcrumbs[i].textContent);
 				}
-				
-				console.log('breads = ', breads);
-				console.log('populating lookFor');
+
 				var lookFor = []; //array holding dir paths to look for install.rdf at. for in the zip
 				for (var i=0; i<breads.length; i++) {
 					var thisLookFor = breads.slice(0, i+1).join('/') + '/';
 					lookFor.push(thisLookFor);
-					console.log('pushing into lookFor = ', thisLookFor);
 				}
-				lookFor.reverse(); //reverse it because we want to find dir with install.rdf closest to filepath dir
-				console.log('DONE populating lookFor');
+				lookFor.reverse();
 				
 				btn.setAttribute('path', breads.join('/') + '/');
 				
@@ -515,36 +415,14 @@ function onPageLoad(doc) {
 							let zipReader = Cc["@mozilla.org/libjar/zip-reader;1"]
 									.createInstance(Ci.nsIZipReader);
 
-							let oFile = FileUtils.getFile("TmpD", [addon.tag+'.xpi']);
-							console.log('pre zip open');
-							console.log('btn.getAttribute(path) = ', btn.getAttribute('path'));
-							//*/ //*/release/
-							
+							let oFile = FileUtils.getFile("TmpD", [addon.tag+'.xpi']);							
 							
 							zipReader.open(nFile);
-							
-							console.log('zip opened');
 							
 							var entries = zipReader.findEntries(null);
 							while(entries.hasMore()) {
 								let entryFileName = entries.getNext();
-									/*
-										string
-									*/
 								let entryZipFile = zipReader.getEntry(entryFileName);
-									/*
-										CRC32:1312936884
-										QueryInterface:QueryInterface()
-										compression:0
-										isDirectory:false
-										isSynthetic:false
-										lastModifiedTime:1398298243000000
-										permissions:256
-										realSize:6547
-										size:6547
-										__proto__:Object
-									*/
-								console.info(entryFileName, typeof(entryFileName), entryZipFile);
 							}
 							
 							
@@ -553,7 +431,6 @@ function onPageLoad(doc) {
 								if(entries.hasMore()) {
 									let entryFileName = entries.getNext();
 									let entryZipFile = zipReader.getEntry(entryFileName);
-									console.info('lookFor[' + i + '] was FOUND so button attribute set', 'lookFor[i] = ', lookFor[i], entryFileName, entryZipFile);
 									btn.setAttribute('path', lookFor[i]);
 									btn.classList.remove('danger');
 									btn.classList.remove('disabled');
@@ -561,9 +438,7 @@ function onPageLoad(doc) {
 									f.className = f.className.replace('hourglass', 'plus');
 									break;
 								} else {
-									console.info('lookFor[' + i + '] was NOT found', 'lookFor[i] = ', lookFor[i]);
 									if (i == lookFor.length -1) {
-										console.log('no install.rdf found in parent dirs of filepath so this is not an installable xpi so removing button');
 										btn.parentNode.removeChild(btn);
 									}
 								}
@@ -571,13 +446,7 @@ function onPageLoad(doc) {
 
 							zipReader.close();
 							
-							//nFile.remove(!1); //should probably change to use OS.File so should be OS.File.remove(nFile.path);
-							var promiseRemove = OS.File.remove(nFile.path);
-							promiseRemove.then(
-								function onsuc() {
-									console.log('succesfully deleted file', nFile.path);
-								}
-							);
+							OS.File.remove(nFile.path);
 						}
 					});
 				});
